57033efe3307061b2a1cf69acf8f729dfb2f1967
l00365 <l00365@163.com>
2021-04-07 22:45:34 +0800

    初始化代码

---
+++ demo.js
@@ -0,0 +1,7 @@
+window.onload=function(){
+	let box=document.getElementsByClassName("box"); //lyy 获取到的是个多元素的数组，所以下面要加[0]
+	box[0].addEventListener('click' ,()=>{
+		alert('这是测试代码');
+	})
+	console.log('这是demo1-imooc');
+}
